9f0ed415d50eea0f3460e18ec7f0b6d90107636e
Roj <ez@roj.im>
2023-03-20 00:52:44 +0300

    Update classno

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export default function classno(className: string, _styl: string) {
-  return className;
+export default function classno(arr: TemplateStringsArray | string[]): void;
+export default function classno(
+  arr: TemplateStringsArray | string[],
+  className: string,
+): string;
+export default function classno(
+  arr: TemplateStringsArray | string[],
+  className?: string,
+) {
+  if (arr[0] != "" && arr.includes("")) {
+    throw new Error("Invalid syntax");
+  }
+
+  if (arr.includes("")) {
+    return className;
+  }
 }
